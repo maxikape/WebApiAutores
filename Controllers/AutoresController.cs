68ef06a30be194b01f5fb81dd00aeecfecb18c61
Tomás Cappelletti <kape2254@gmail.com>
2023-01-16 20:24:58 -0300

    inicial

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext contex;
+
+        public AutoresController(ApplicationDbContext contex)
+        {
+            this.contex = contex;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await contex.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await contex.Autores.FirstOrDefaultAsync();
+        }
+
+
+        [HttpGet("{id:int}/{Param2=persona}")]
+        public async Task<ActionResult<Autor>> Get(int id, string persona)
+        {
+
+                var autor = await contex.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (autor == null)
+                {
+                    return NotFound();
+                }
+
+                return autor;
+            
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            contex.Add(autor);
+            await contex .SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id) 
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("el id del Autor No coincide con el ID de la URL");
+            }
+
+            contex.Update(autor);
+            await contex.SaveChangesAsync();
+            return Ok();
+
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await contex.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            contex.Remove(new Autor {Id = id });
+            await contex.SaveChangesAsync();
+            return Ok();
+
+
+        }
+
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Text.Json.Serialization;
+
+namespace WebApiAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers().AddJsonOptions(x => 
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            services.AddDbContext<ApplicationDbContext>(Options =>
+                Options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllers();
+
+            });
+
+
+        }
+
+
+    }
+}
